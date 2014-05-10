14abb334318b9e9a0700d50bc3c1b280dd76c6a7
xboy2012 <xb2012@gmail.com>
2014-05-10 16:22:46 +0800

    test

--- main.php
+++ main.php
@@ -3,12 +3,14 @@
 <head>
 <meta name="apple-touch-fullscreen" content="yes" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="default" /> 
-<meta content="telephone=no" name="format-detection" />
-<meta content="email=no" name="format-detection" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" /> 
+<meta name="format-detection" content="telephone=no" />
+<meta name="format-detection" content="email=no" />
+<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon.png" />
 <title>XBOY</title>
 </head>
 <body>
-123
+<h1>Hello, iOS!</h1>
+<input type="button" value="Button" onclick="alert('XBOY')" />
 </body>
 </html>
