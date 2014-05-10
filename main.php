c458640a9e77ad066a063b08c27a786c07b95635
xboy2012 <xb2012@gmail.com>
2014-05-10 16:04:36 +0800

    test

--- main.php
+++ main.php
@@ -4,6 +4,8 @@
 <meta name="apple-touch-fullscreen" content="yes" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" /> 
+<meta content="telephone=no" name="format-detection" />
+<meta content="email=no" name="format-detection" />
 <title>XBOY</title>
 </head>
 <body>
