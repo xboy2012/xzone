e1544373b518b4908774f8cc347056260e6714df
xboy2012 <xb2012@gmail.com>
2014-05-10 16:31:33 +0800

    set auto width

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width" />
 <meta name="apple-touch-fullscreen" content="yes" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black" /> 
